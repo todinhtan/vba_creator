2d227aaed33d58b248cc11a7a2f7e82ac49947a0
Tan <tan@intersquad.co>
2019-04-24 23:47:30 +0700

    add pull transfers from wyre and add them into epiapi

---
+++ create_transfer_queue.py
@@ -0,0 +1,89 @@
+from lib.wyre import wyre
+import logging, graypy
+import os, json
+from requests import get
+from pymongo import MongoClient
+from lib.vba import vba
+
+grayLogger = logging.getLogger('graylog')
+grayLogger.setLevel(logging.CRITICAL)
+handler = graypy.GELFHandler(os.environ['GRAYLOG_HOST'], int(os.environ['GRAYLOG_PORT']))
+grayLogger.addHandler(handler)
+
+mongoClient = MongoClient(os.environ['VBA_DB_HOST'], int(os.environ['VBA_DB_PORT']))
+db = mongoClient.vba_service
+
+def createWyreApi(credentials):
+    account_id = credentials.get('accountId', None)
+    api_key = credentials.get('apiKey', None)
+    secret_key = credentials.get('apiSecKey', None)
+    return wyre(account_id, 'v3', api_key, secret_key)
+
+wyreCli = createWyreApi({
+    "accountId": os.environ['WYRE_ADMIN_ACCOUNTID'],
+    "apiKey": os.environ['WYRE_ADMIN_APIKEY'],
+    "apiSecKey": os.environ['WYRE_ADMIN_SECRET']
+})
+
+vbaCli = vba()
+
+def getWalletIdByUserId(userId):
+    vba = db.vbarequests.find_one({'country': 'US', 'vbaData.userId': userId})
+    return vba.get('walletId') if vba is not None else None
+
+def insertQueuedTranfer(document):
+    newDoc = db.daily_topup_transfers.insert_one(document)
+    return newDoc
+
+def isQueuedTranferExisted(wyreTransferId):
+    total = db.daily_topup_transfers.count_documents({'wyreTransferId': wyreTransferId})
+    print(total)
+    return total > 0
+
+def process():
+    http_code, trans_info = wyreCli.get_trans_info()
+    if http_code == 200:
+        transfers = trans_info['data']
+        if transfers is not None and len(transfers) > 0:
+            for transfer in transfers:
+                # skip if source != 'service:Fiat Credits'
+                if transfer['source'] != 'service:Fiat Credits':
+                    continue
+                
+                # skip if status != 'COMPLETED'
+                if transfer['status'] != 'COMPLETED':
+                    continue
+                
+                # skip if sourceAmount != destAmount
+                if transfer['sourceAmount'] != transfer['destAmount']:
+                    continue
+
+                transferId = transfer['id']
+                # skip if it has been added to queue
+                if isQueuedTranferExisted(transferId):
+                    continue
+
+                # hardcode message = userId
+                # message = transfer['message']
+                message = '5c9afc555ac64800661b190a'
+                http_status, response = vbaCli.addFunds(transfer['sourceAmount'], message, transfer['sourceCurrency'], transfer['destCurrency'])
+                if http_status == 200:
+                    newTransfer = json.loads(response)
+                    walletId = getWalletIdByUserId(message)
+                    document = {
+                        'transferId': newTransfer['transfer']['id'],
+                        'wyreTransferId': transfer['id'],
+                        'source': transfer['source'],
+                        'sourceCurrency': transfer['sourceCurrency'],
+                        'destCurrency': transfer['destCurrency'],
+                        'sourceAmount': transfer['sourceAmount'],
+                        'destAmount': transfer['destAmount'],
+                        'dest': 'wallet:{}'.format(walletId),
+                        'message': 'addenda',
+                        'autoConfirm': False,
+                        'status': 'PENDING'
+                    }
+                    insertQueuedTranfer(document)
+
+if __name__ == '__main__':
+    process()
